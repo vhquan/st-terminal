fc64c32704029f1e0bab465054f638f2dbb7de0e
vhquan <vhquan.100@gmail.com>
2020-05-31 07:15:12 +0700

    Bigger size, default theme

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -145,8 +145,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 120;
+static unsigned int rows = 35;
 
 /*
  * Default colour and shape of the mouse cursor
