ab4e3877d980c673a515525966623b39da602647
vhquan <vhquan.100@gmail.com>
2020-05-31 22:30:34 +0700

    init source code, guide in README, feature patchs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # suckless-st
+
+Apply nord theme: patch < patch/st-nordtheme-0.8.2.diff
--- config.def.h
+++ config.def.h
@@ -93,29 +93,44 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-"#2e3440", /* base00 */
-"#88c0d0", /* base08 */
-"#bf616a", /* base0B */
-"#5e81ac", /* base0A */
-"#ebcb8b", /* base0D */
-"#a3be8c", /* base0E */
-"#d08770", /* base0C */
-"#e5e9f0", /* base05 */
-"#4c566a", /* base03 */
-"#81a1c1", /* base09 */
-"#3b4252", /* base01 */
-"#434c5e", /* base02 */
-"#d8dee9", /* base04 */
-"#eceff4", /* base06 */
-"#b48ead", /* base0F */
-"#8fbcbb", /* base07 */
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
 };
 
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 13;
-static unsigned int defaultrcs = 0;
+static unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
